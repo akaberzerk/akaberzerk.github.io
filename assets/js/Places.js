ab03334750bdd87954353895780e98b6e1cced68
Ashish Shrestha <59525462+akaberzerk@users.noreply.github.com>
2023-10-11 12:08:49 +0545

    Add files via upload
    updated resume

---
+++ assets/js/Places.js
@@ -0,0 +1,23 @@
+var PlacesJson = {
+"type": "FeatureCollection",
+"name": "Places",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "SN": 1, "Name": "Bheemdatta", "By_": "Plane", "Lat": 28.9873, "Long": 80.1652 }, "geometry": { "type": "Point", "coordinates": [ 80.1652, 28.9873 ] } },
+{ "type": "Feature", "properties": { "SN": 2, "Name": "Lukla", "By_": "Trek", "Lat": 27.6857, "Long": 86.7278 }, "geometry": { "type": "Point", "coordinates": [ 86.7278, 27.6857 ] } },
+{ "type": "Feature", "properties": { "SN": 3, "Name": "Poon Hill", "By_": "Trek", "Lat": 28.4002, "Long": 83.6893 }, "geometry": { "type": "Point", "coordinates": [ 83.6893, 28.4002 ] } },
+{ "type": "Feature", "properties": { "SN": 4, "Name": "Lo Manthang", "By_": "Bike", "Lat": 29.208, "Long": 83.8685 }, "geometry": { "type": "Point", "coordinates": [ 83.8685, 29.208 ] } },
+{ "type": "Feature", "properties": { "SN": 5, "Name": "Kathmandu", "By_": "Capital City", "Lat": 27.7172, "Long": 85.324 }, "geometry": { "type": "Point", "coordinates": [ 85.324, 27.7172 ] } },
+{ "type": "Feature", "properties": { "SN": 6, "Name": "Pokhara", "By_": "Home City", "Lat": 28.2096, "Long": 83.9856 }, "geometry": { "type": "Point", "coordinates": [ 83.9856, 28.2096 ] } },
+{ "type": "Feature", "properties": { "SN": 7, "Name": "Muktinath", "By_": "Bike", "Lat": 28.8017, "Long": 83.8579 }, "geometry": { "type": "Point", "coordinates": [ 83.8579, 28.8017 ] } },
+{ "type": "Feature", "properties": { "SN": 8, "Name": "Jomsom", "By_": "Bike", "Lat": 28.9985, "Long": 83.8473 }, "geometry": { "type": "Point", "coordinates": [ 83.8473, 28.9985 ] } },
+{ "type": "Feature", "properties": { "SN": 9, "Name": "Darjeeling", "By_": "Bus", "Lat": 27.041, "Long": 88.2663 }, "geometry": { "type": "Point", "coordinates": [ 88.2663, 27.041 ] } },
+{ "type": "Feature", "properties": { "SN": 10, "Name": "Sikkim", "By_": "Bus", "Lat": 27.3516, "Long": 88.3239 }, "geometry": { "type": "Point", "coordinates": [ 88.3239, 27.3516 ] } },
+{ "type": "Feature", "properties": { "SN": 11, "Name": "Ilam", "By_": "Bus", "Lat": 26.9112, "Long": 87.9237 }, "geometry": { "type": "Point", "coordinates": [ 87.9237, 26.9112 ] } },
+{ "type": "Feature", "properties": { "SN": 12, "Name": "Burtibang", "By_": "Bus", "Lat": 28.3319, "Long": 83.1602 }, "geometry": { "type": "Point", "coordinates": [ 83.1602, 28.3319 ] } },
+{ "type": "Feature", "properties": { "SN": 13, "Name": "Dharan", "By_": "Bus", "Lat": 26.8065, "Long": 87.2846 }, "geometry": { "type": "Point", "coordinates": [ 87.2846, 26.8065 ] } },
+{ "type": "Feature", "properties": { "SN": 14, "Name": "Sindhuligadhi", "By_": "Bus", "Lat": 27.2569, "Long": 85.9713 }, "geometry": { "type": "Point", "coordinates": [ 85.9713, 27.2569 ] } },
+{ "type": "Feature", "properties": { "SN": 15, "Name": "Lumbini", "By_": "Bus", "Lat": 27.4833, "Long": 83.2764 }, "geometry": { "type": "Point", "coordinates": [ 83.2764, 27.4833 ] } },
+{ "type": "Feature", "properties": { "SN": 16, "Name": "Janakpur", "By_": "Bus", "Lat": 26.7271, "Long": 85.9407 }, "geometry": { "type": "Point", "coordinates": [ 85.9407, 26.7271 ] } }
+]
+}
